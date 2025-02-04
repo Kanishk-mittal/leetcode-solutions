465760ba9428129b0307b925abfb9f27baaf898b
Kanishk-mittal <kanishkmittal2005@gmail.com>
2025-02-04 08:44:23 +0530

    Implement solution for Maximum Ascending Subarray Sum problem

---
+++ 1800_Maximum_Ascending_Subarray_Sum.cpp
@@ -0,0 +1,35 @@
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maxAscendingSum(vector<int> &nums)
+    {
+        // using sliding window approacn return the maximum sum of a strictly increasing subarray
+        int n = nums.size();
+        int sum = nums[0];
+        int max_sum = nums[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                sum += nums[i];
+            }
+            else
+            {
+                sum = nums[i];
+            }
+            max_sum = max(max_sum, sum);
+        }
+        return max_sum;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
