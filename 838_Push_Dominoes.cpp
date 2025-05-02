9e9a189b45f0a70558697a1409c081a55f43edef
Kanishk-mittal <kanishkmittal2005@gmail.com>
2025-05-02 19:10:33 +0530

    Implement solution for Push Dominoes problem

---
+++ 838_Push_Dominoes.cpp
@@ -0,0 +1,59 @@
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string pushDominoes(string dominoes)
+    {
+        int n = dominoes.size();
+        vector<int> forces(n, 0);
+
+        // Left to right
+        int force = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (dominoes[i] == 'R')
+                force = n;
+            else if (dominoes[i] == 'L')
+                force = 0;
+            else
+                force = max(force - 1, 0);
+            forces[i] += force;
+        }
+
+        // Right to left
+        force = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (dominoes[i] == 'L')
+                force = n;
+            else if (dominoes[i] == 'R')
+                force = 0;
+            else
+                force = max(force - 1, 0);
+            forces[i] -= force;
+        }
+
+        // Build result
+        string result;
+        for (int f : forces)
+        {
+            if (f > 0)
+                result += 'R';
+            else if (f < 0)
+                result += 'L';
+            else
+                result += '.';
+        }
+        return result;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
