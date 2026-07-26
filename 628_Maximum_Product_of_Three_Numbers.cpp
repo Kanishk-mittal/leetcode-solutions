bf4bb7abc136c9f12a536c958d7a4723c3256061
Kanishk-mittal <kanishkmittal2005@gmail.com>
2026-07-26 21:00:49 +0530

    Add implementation for Maximum Product of Three Numbers problem

---
+++ 628_Maximum_Product_of_Three_Numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maximumProduct(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return max(nums[0] * nums[1] * nums[n - 1], nums[n - 3] * nums[n - 2] * nums[n - 1]);
+    }
+};
+
+int main()
+{
+    Solution sol;
+    return 0;
+}
