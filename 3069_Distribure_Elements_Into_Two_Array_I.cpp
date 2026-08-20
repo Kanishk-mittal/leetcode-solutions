2b381fd18f86e208b9bbbbccc18f5059e283b2c5
Kanishk-mittal <kanishkmittal2005@gmail.com>
2026-08-20 09:05:42 +0530

    Add implementation for Distribute Elements Into Two Arrays problem

---
+++ 3069_Distribure_Elements_Into_Two_Array_I.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> resultArray(vector<int> &nums)
+    {
+        if (nums.size() <= 2)
+        {
+            return nums;
+        }
+
+        int n = nums.size();
+        vector<int> arr1;
+        vector<int> arr2;
+        arr1.reserve(n);
+        arr2.reserve(n);
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+        for (int i = 2; i < n; i++)
+        {
+            if (arr1.back() > arr2.back())
+            {
+                arr1.push_back(nums[i]);
+            }
+            else
+            {
+                arr2.push_back(nums[i]);
+            }
+        }
+        arr1.insert(arr1.end(), arr2.begin(), arr2.end());
+        return arr1;
+    }
+};
+auto init = []()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
